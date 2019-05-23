0789350a6a7b578210c9040ab9898f489b7fa48e
[benjymarks] <[benjy.marks@sydney.edu.au]>
2019-05-23 18:00:07 +1000

    Bumped segregation rate for square drum

--- inputs/bi_square_drum.py
+++ inputs/bi_square_drum.py
@@ -17,7 +17,7 @@ class Params():
         self.O = Output_Params(self.G)#self.nt)
         self.S = [Solid_Params(self.G,self,args),]
         self.segregate_grid = True
-        self.c = 1e-3 # inter-particle drag coefficient
+        self.c = 1e-2 # inter-particle drag coefficient
         self.D = 0. # segregation diffusion coefficient
         self.supername = 'im/drum/ny_' + str(self.G.ny) + '/Fr_' + str(self.Fr) + '/'
         self.pressure = 'lithostatic'
