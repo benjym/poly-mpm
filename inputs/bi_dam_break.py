ad80cb82bc93cb1cad64266dd3b074045fabc34a
benjym <benjy.marks@sydney.edu.au>
2019-10-15 13:58:36 +1100

    went back to proper polydisperse discretisation for dam break

--- inputs/bi_dam_break.py
+++ inputs/bi_dam_break.py
@@ -51,9 +51,9 @@ class Grid_Params():
         self.s_m = self.s_M/self.R
         self.ns = int(args[4])
         # self.s = array([self.s_m,self.s_M])
-        # s_edges = linspace(self.s_m,self.s_M,self.ns+1)
-        # self.s = (s_edges[1:] + s_edges[:-1])/2.
-        self.s = linspace(self.s_m,self.s_M,self.ns)
+        s_edges = linspace(self.s_m,self.s_M,self.ns+1)
+        self.s = (s_edges[1:] + s_edges[:-1])/2.
+        # self.s = linspace(self.s_m,self.s_M,self.ns)
         self.ds = self.s[1]-self.s[0]
 
 class Boundary_Params():
